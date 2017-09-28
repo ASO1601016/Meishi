bf03a0cceb1c27aab4a2875ae29b5fffc35bb1c7
ASO1601016 <32320650+ASO1601016@users.noreply.github.com>
2017-09-28 10:40:25 +0900

    初めてのコミット

--- app/src/main/java/com/example/akasoo/meishi/MainActivity.java
+++ app/src/main/java/com/example/akasoo/meishi/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //a
 
     Button tapHere = (Button)findViewById(R.id.tapHere);
     tapHere.setOnClickListener(new View.OnClickListener(){
