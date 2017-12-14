38c35fd0788e6440950b1973a0aa9a71f59bef7e
ASO1601016 <32320650+ASO1601016@users.noreply.github.com>
2017-12-14 10:52:16 +0900

    こみっと

--- app/src/main/java/com/example/akasoo/meishi/MainActivity.java
+++ app/src/main/java/com/example/akasoo/meishi/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
